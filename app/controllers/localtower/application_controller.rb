731a6bc03aaf361f3cead0e85578e0cd0da810bd
Damian Le Nouaille <dam@dln.name>
2017-03-14 21:08:08 +0100

    check environment

--- app/controllers/localtower/application_controller.rb
+++ app/controllers/localtower/application_controller.rb
@@ -1,5 +1,10 @@
 module Localtower
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :null_session
+    before_action :check_environment
+
+    def check_environment
+      raise if Rails.env.production?
+    end
   end
 end
