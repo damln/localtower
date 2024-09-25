ba765a4b8e8394370a177a3e4ae98753fcbb70af
Damian Le Nouaille <damlenouaille@gmail.com>
2024-09-25 16:57:27 +0200

    small clean

--- lib/localtower.rb
+++ lib/localtower.rb
@@ -5,12 +5,6 @@ end
 require 'thor'
 require 'active_link_to'
 
-begin
-  require "pry"
-rescue Exception => e
-  # Nothing
-end
-
 root = File.expand_path(File.dirname(__FILE__))
 Dir["#{root}/localtower/*.rb"].sort.each { |file| require file }
 Dir["#{root}/localtower/*/**/*.rb"].each { |file| require file }
